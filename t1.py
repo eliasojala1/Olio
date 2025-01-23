14e8a6bb7bce1164ebd0ee894c16d0f680b74409
eliasojala1 <herelk3@gmail.com>
2025-01-23 19:32:21 +0200

    Task 3 done

--- t1.py
+++ t1.py
@@ -11,3 +11,11 @@ print("original strings:", Strings)
 Numbers = [random.randint(1, 100) for _ in range(10)]
 
 print("Random numbers:", Numbers)
+
+#t3
+
+Numbers.sort()
+Strings.sort()
+
+print("Arranged numers:", Numbers)
+print("Arranged strings:", Strings)
