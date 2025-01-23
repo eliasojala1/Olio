c0d4da7d30751415bccc0acf41133af21de33629
eliasojala1 <herelk3@gmail.com>
2025-01-23 20:40:59 +0200

    task 10 done

---
+++ t.1/t.1.10.py
@@ -0,0 +1,33 @@
+def main():
+
+    book = {}
+
+    while True:
+
+        command = int(input("1 = search, 2 = add, 3 = quit: "))
+
+        if command == 1: #search
+            name = input("Give a name: ")
+            if name in book:
+                print(book[name])
+            else:
+                print("not found")
+
+
+        elif command == 2: #add
+            name = input("Give a name: ")            
+            number = input("Give a number: ")
+            book[name] = number
+            print("contact added")        
+
+
+        elif command == 3: #quit
+            print("Goodbye!")
+            break
+
+
+        else:
+            print("something went wrong, give number from 1 to 3")
+
+
+main()
