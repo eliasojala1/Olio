46a57d322180166bf8257402833aa173518214d0
eliasojala1 <herelk3@gmail.com>
2025-01-23 19:29:15 +0200

    Task2

--- t1.py
+++ t1.py
@@ -1,3 +1,13 @@
+import random
+
 #t2
 
-print("hello")
+Numbers = [int(input("Give ten number: ")) for _ in range(10)]
+Strings = [input("Give ten strings: ") for _ in range(10)]
+
+print("original numbers:", Numbers)
+print("original strings:", Strings)
+
+Numbers = [random.randint(1, 100) for _ in range(10)]
+
+print("Random numbers:", Numbers)
