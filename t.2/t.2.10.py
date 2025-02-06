3a462da227464422e007dd87dde11440563fda62
eliasojala1 <herelk3@gmail.com>
2025-02-07 01:19:22 +0200

    small adding

--- t.2/t.2.10.py
+++ t.2/t.2.10.py
@@ -17,7 +17,7 @@ apt1 = Property(1, 16, 5500)
 apt2 = Property(2, 38, 4200)
 apt3 = Property(3, 78, 2500)
 
-print("\nSize difference:")
+print("\nIs it bigger:")
 print(apt1.bigger(apt2))
 print(apt3.bigger(apt2))
 
@@ -25,6 +25,6 @@ print("\nPrice difference:")
 print(apt1.price_diff(apt2))
 print(apt3.price_diff(apt2))
 
-print("\nPrice difference:")
+print("\nIs it more expensive:")
 print(apt1.more_exp(apt2))
 print(apt3.more_exp(apt2), "\n")
