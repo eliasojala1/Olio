5a671a49aebac4af1b583220fd793a4cfaa1b939
eliasojala1 <herelk3@gmail.com>
2025-02-07 00:51:10 +0200

    t.10

---
+++ t.2/t.2.10.py
@@ -0,0 +1,27 @@
+class Property:
+    def __init__(self, rooms, size, price):
+        self.rooms = rooms
+        self.size = size
+        self.price = price
+
+    def bigger(self, other):
+        return self.size > other.size
+
+    def price_diff(self, other):
+        return abs((self.size * self.price) - (other.size * other.price))
+
+    def more_exp(self, other):
+        return (self.size * self.price) > (other.size * other.price)
+
+apt1 = Property(1, 16, 5500)
+apt2 = Property(2, 38, 4200)
+apt3 = Property(3, 78, 2500)
+
+print(apt1.bigger(apt2))
+print(apt3.bigger(apt2))
+
+print(apt1.price_diff(apt2))
+print(apt3.price_diff(apt2))
+
+print(apt1.more_exp(apt2))
+print(apt3.more_exp(apt2))
